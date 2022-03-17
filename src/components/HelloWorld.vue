b6ef5505c7df981980204460a20732cf7b2b4ca0
baizx98 <baizx2020@outlook.com>
2022-03-17 16:45:07 +0800

    add 九尾妖狐不害怕

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" @click="count++">我说了{{ count }}次九尾妖狐一点都不害怕</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
