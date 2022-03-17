8e65a3151684585088e956f8ce3604e6b54a87ba
baizx98 <baizx2020@outlook.com>
2022-03-17 20:17:43 +0800

    add element plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,14 +19,48 @@ const count = ref(0)
   </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
   <button type="button" @click="count++">我说了{{ count }}次九尾妖狐一点都不害怕</button>
+  <!-- <el-button>这就是element plus咩</el-button> -->
+  <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button plain>朴素按钮</el-button>
+    <el-button type="primary" plain>主要按钮</el-button>
+    <el-button type="success" plain>成功按钮</el-button>
+    <el-button type="info" plain>信息按钮</el-button>
+    <el-button type="warning" plain>警告按钮</el-button>
+    <el-button type="danger" plain>危险按钮</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button round>圆角按钮</el-button>
+    <el-button type="primary" round>主要按钮</el-button>
+    <el-button type="success" round>成功按钮</el-button>
+    <el-button type="info" round>信息按钮</el-button>
+    <el-button type="warning" round>警告按钮</el-button>
+    <el-button type="danger" round>危险按钮</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button icon="el-icon-search" circle></el-button>
+    <el-button type="primary" icon="el-icon-edit" circle></el-button>
+    <el-button type="success" icon="el-icon-check" circle></el-button>
+    <el-button type="info" icon="el-icon-message" circle></el-button>
+    <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+    <el-button type="danger" icon="el-icon-delete" circle></el-button>
+  </el-row>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
